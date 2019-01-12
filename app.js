60ee08a3de2ae4bec95333af07b5c1ba6b185125
Gambit90s <44451389+Gambit90s@users.noreply.github.com>
2019-01-12 13:20:04 -0700

    Dropped in Files

---
+++ app.js
@@ -0,0 +1,68 @@
+let cells = document.querySelectorAll('.row div');
+let currentPlayer = 'X';
+cells.forEach(function(cell){
+    cell.addEventListener('click', cellClicked);
+});
+
+function cellClicked(e) {
+
+    if(e.target.textContent !== '') {
+        return;
+    }
+    e.target.textContent = currentPlayer;
+    checkGame();
+    togglePlayer();
+};
+
+function checkGame() {
+    if(checkTop()) {
+        return true;
+    } else {
+        return false;
+    
+}
+
+}
+ 
+function checkTop() {
+    if( cells[0].textContent === cells[1].textContent && cell[1].textContent === cell[2].textContent && isEmpty(0, 1, 2)){
+        return true;
+    }   else {
+        return false;
+    }
+}
+
+function checkMiddle() {
+    if( cells[3].textContent === cells[3].textContent && cell[4].textContent === cell[5].textContent && isEmpty(3, 4, 5)){
+        return true;
+    }   else {
+        return false;
+    }
+}
+
+function checkBottom() {
+    if( cells[6].textContent === cells[6].textContent && cell[7].textContent === cell[8].textContent && isEmpty(6, 7, 8)){
+        return true;
+    }   else {
+        return false;
+    }
+}
+
+function isEmpty(a, b, c) {
+    if(cells[a].textContent === "" || cells[b].textContent === "" || cells[c].textContent === "") {
+        return true;
+    } else { 
+        return false;
+    }
+
+
+
+}
+
+function togglePlayer() {
+    if(currentPlayer === 'X') {
+        currentPlayer = 'O';
+    } else { 
+        currentPlayer = 'X';
+    }
+}
